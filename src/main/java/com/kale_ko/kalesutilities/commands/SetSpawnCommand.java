0e18dd374fcd2d9ea770c60826eed63581c4c237
Kale-Ko <github@kaleko.dev>
2021-12-20 14:07:25 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/commands/SetSpawnCommand.java
+++ src/main/java/com/kale_ko/kalesutilities/commands/SetSpawnCommand.java
@@ -7,26 +7,32 @@ import java.nio.file.Paths;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
 public class SetSpawnCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (Util.hasPermission(sender, "kalesutilities.setspawn")) {
-            if (args.length > 0) {
-                File dataFolder = Main.Instance.getDataFolder();
-                if (!dataFolder.exists()) {
-                    dataFolder.mkdir();
-                }
+            File dataFolder = Main.Instance.getDataFolder();
+            if (!dataFolder.exists()) {
+                dataFolder.mkdir();
+            }
 
-                File dataFile = Paths.get(dataFolder.getAbsolutePath(), "spawn.yml").toFile();
+            File dataFile = Paths.get(dataFolder.getAbsolutePath(), "spawn.yml").toFile();
 
-                YamlConfiguration data = YamlConfiguration.loadConfiguration(dataFile);
+            YamlConfiguration data = YamlConfiguration.loadConfiguration(dataFile);
 
+            if (sender instanceof Player player) {
+                data.set(player.getWorld() + ".x", player.getLocation().getX());
+                data.set(player.getWorld() + ".y", player.getLocation().getY());
+                data.set(player.getWorld() + ".z", player.getLocation().getZ());
+                data.set(player.getWorld() + ".pitch", player.getLocation().getPitch());
+                data.set(player.getWorld() + ".yaw", player.getLocation().getYaw());
             } else {
-                Util.sendMessage(sender, Main.Instance.config.getString("messages.usage").replace("{usage}", Main.Instance.getCommand("setspawn").getUsage()));
+                Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
             }
         } else {
-            Util.sendMessage(sender, Main.Instance.config.getString("messages.noperms").replace("{permission}", "kalesutilities.setspawn"));
+            Util.sendMessage(sender, Main.Instance.config.getString("messages.usage").replace("{usage}", Main.Instance.getCommand("setspawn").getUsage()));
         }
 
         return true;
