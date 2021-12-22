c876aa9bd42e16aaeb7a796b7aa6d44a4ec82f42
Kale-Ko <github@kaleko.dev>
2021-12-22 17:59:51 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/Main.java
+++ src/main/java/com/kale_ko/kalesutilities/Main.java
@@ -28,7 +28,7 @@ import org.bukkit.plugin.java.annotation.plugin.LoadOrder;
 import org.bukkit.configuration.file.FileConfiguration;
 import java.util.logging.Logger;
 
-@Plugin(name = "KalesUtilities", version = "1.0.0")
+@Plugin(name = "KalesUtilities", version = "1.3.1")
 @Description("A custom plugin to run on KalesMC")
 @Author("Kale_Ko")
 @LogPrefix("Kales Utilities")
