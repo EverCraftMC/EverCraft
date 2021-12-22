8a47ac4e995e7e418e6c2454309c192bf490e2cb
Kale-Ko <github@kaleko.dev>
2021-12-22 10:07:01 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/commands/NicknameCommand.java
@@ -0,0 +1,45 @@
+package com.kale_ko.kalesutilities.commands;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+public class NicknameCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (Util.hasPermission(sender, "kalesutilities.setnickname")) {
+            File dataFolder = Main.Instance.getDataFolder();
+            if (!dataFolder.exists()) {
+                dataFolder.mkdir();
+            }
+
+            File dataFile = Paths.get(dataFolder.getAbsolutePath(), "players.yml").toFile();
+
+            YamlConfiguration data = YamlConfiguration.loadConfiguration(dataFile);
+
+            if (sender instanceof Player player) {
+                data.set("players." + player.getPlayer().getName() + ".nickname", args[0]);
+
+                try {
+                    data.save(dataFile);
+
+                    Util.sendMessage(sender, Main.Instance.config.getString("messages.setnickname"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
+            }
+        } else {
+            Util.sendMessage(sender, Main.Instance.config.getString("messages.usage").replace("{usage}", Main.Instance.getCommand("nickname").getUsage()));
+        }
+
+        return true;
+    }
+}
