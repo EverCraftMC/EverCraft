09aacf38192ffe7ad074b83671aad456cfbb84ba
Kale-Ko <github@kaleko.ga>
2021-12-22 14:17:36 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/listeners/ChatFormatListener.java
+++ src/main/java/com/kale_ko/kalesutilities/listeners/ChatFormatListener.java
@@ -2,9 +2,6 @@ package com.kale_ko.kalesutilities.listeners;
 
 import com.kale_ko.kalesutilities.Main;
 import com.kale_ko.kalesutilities.Util;
-import java.io.File;
-import java.nio.file.Paths;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
@@ -13,30 +10,9 @@ import org.bukkit.event.player.PlayerChatEvent;
 public class ChatFormatListener implements Listener {
     @EventHandler
     public void onChatMessage(PlayerChatEvent event) {
-        File dataFolder = Main.Instance.getDataFolder();
-        if (!dataFolder.exists()) {
-            dataFolder.mkdir();
-        }
-
-        File dataFile = Paths.get(dataFolder.getAbsolutePath(), "players.yml").toFile();
-
-        YamlConfiguration data = YamlConfiguration.loadConfiguration(dataFile);
-
-        String player = event.getPlayer().getName();
-        String prefix = "";
+        String player = Util.getPlayerNickName(event.getPlayer());
+        String prefix = Util.getPlayerPrefix(event.getPlayer());
         String message = event.getMessage();
-
-        if (data.getString("players." + event.getPlayer().getName() + ".nickname") != null && !data.getString("players." + event.getPlayer().getName() + ".nickname").equalsIgnoreCase("") && !data.getString("players." + event.getPlayer().getName() + ".nickname").equalsIgnoreCase(" ")) {
-            if (Util.hasPermission(event.getPlayer(), "kalesutilities.nonickstar") || data.getString("players." + event.getPlayer().getName() + ".nickname").equalsIgnoreCase(player) || Util.stripFormating(Util.formatMessage(data.getString("players." + event.getPlayer().getName() + ".nickname") + "&r")).equalsIgnoreCase(player)) {
-                player = Util.formatMessage(data.getString("players." + event.getPlayer().getName() + ".nickname") + "&r");
-            } else {
-                player = Util.formatMessage("*" + data.getString("players." + event.getPlayer().getName() + ".nickname") + "&r");
-            }
-        }
-
-        if (data.getString("players." + event.getPlayer().getName() + ".prefix") != null && !data.getString("players." + event.getPlayer().getName() + ".prefix").equalsIgnoreCase("") && !data.getString("players." + event.getPlayer().getName() + ".prefix").equalsIgnoreCase(" ")) {
-            prefix = Util.formatMessage(data.getString("players." + event.getPlayer().getName() + ".prefix") + "&r") + " ";
-        }
 
         if (Util.hasPermission(event.getPlayer(), "kalesutilities.colorchat")) {
             message = Util.formatMessage(event.getMessage());
