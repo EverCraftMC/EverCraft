59d9f51342db9c34b8c6a71571b6efa553811348
Kale-Ko <github@kaleko.dev>
2021-12-22 19:43:08 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/commands/RulesCommand.java
@@ -0,0 +1,19 @@
+package com.kale_ko.kalesutilities.commands;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RulesCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (Util.hasPermission(sender, "kalesutilities.rules")) {
+            Util.sendMessage(sender, Main.Instance.config.getString("config.rules"));
+        } else {
+            Util.sendMessage(sender, Main.Instance.config.getString("messages.noperms").replace("{permission}", "kalesutilities.rules"));
+        }
+
+        return true;
+    }
+}
