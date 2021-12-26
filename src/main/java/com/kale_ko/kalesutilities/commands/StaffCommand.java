002eae4682b1815a92b1c7d7b7242e238ace9c40
Kale-Ko <github@kaleko.dev>
2021-12-26 13:02:29 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/commands/StaffCommand.java
@@ -0,0 +1,19 @@
+package com.kale_ko.kalesutilities.commands;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StaffCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (Util.hasPermission(sender, "kalesutilities.staff")) {
+            Util.sendMessage(sender, Main.Instance.config.getString("config.staff"));
+        } else {
+            Util.sendMessage(sender, Main.Instance.config.getString("messages.noperms").replace("{permission}", "kalesutilities.staff"));
+        }
+
+        return true;
+    }
+}
