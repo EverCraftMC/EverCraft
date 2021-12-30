358dd6e49f47830e83714c45d22fbd936db9f1db
Kale-Ko <github@kaleko.ga>
2021-12-29 22:15:40 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/listeners/MuteListener.java
@@ -0,0 +1,31 @@
+package com.kale_ko.kalesutilities.listeners;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import java.io.File;
+import java.nio.file.Paths;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+
+@SuppressWarnings("deprecation")
+public class MuteListener implements Listener {
+    @EventHandler
+    public void onChatMessage(PlayerChatEvent event) {
+        File dataFolder = Main.Instance.getDataFolder();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdir();
+        }
+
+        File dataFile = Paths.get(dataFolder.getAbsolutePath(), "players.yml").toFile();
+
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(dataFile);
+
+        if (data.getBoolean("players." + event.getPlayer().getName() + ".muted")) {
+            event.setCancelled(true);
+
+            Util.sendMessage(event.getPlayer(), data.getString("players." + event.getPlayer().getName() + ".mutedMessage"));
+        }
+    }
+}
