b23bf15c8a18f918bece9a9e395ad286211a19b6
Kale-Ko <github@kaleko.dev>
2021-12-30 20:53:12 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/listeners/CommandSpyListener.java
@@ -0,0 +1,19 @@
+package com.kale_ko.kalesutilities.listeners;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CommandSpyListener implements Listener {
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        for (Player player : Main.Instance.getServer().getOnlinePlayers()) {
+            if (Util.hasPermission(player, "kalesutilities.commandspy")) {
+                Util.sendMessage(player, Main.Instance.config.getString("messages.commandspy").replace("{player}", Util.getPlayerName(event.getPlayer())).replace("{message}", event.getMessage()), true);
+            }
+        }
+    }
+}
