3b40ecd6b9337d8229d4707cf84abf341ddca60e
Kale-Ko <github@kaleko.ga>
2022-01-08 15:19:23 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/listeners/SpawnListener.java
+++ src/main/java/com/kale_ko/kalesutilities/listeners/SpawnListener.java
@@ -1,7 +1,6 @@
 package com.kale_ko.kalesutilities.listeners;
 
 import com.kale_ko.kalesutilities.Main;
-import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -11,14 +10,14 @@ public class SpawnListener implements Listener {
     @EventHandler
     public void onPlayerSpawn(PlayerSpawnLocationEvent event) {
         if (Main.Instance.spawn.getBoolean("overidespawn")) {
-            event.setSpawnLocation(new Location(event.getSpawnLocation().getWorld(), Main.Instance.spawn.getDouble(event.getSpawnLocation().getWorld().getName() + ".x"), Main.Instance.spawn.getDouble(event.getSpawnLocation().getWorld().getName() + ".y"), Main.Instance.spawn.getDouble(event.getSpawnLocation().getWorld().getName() + ".z"), Float.parseFloat(Main.Instance.spawn.getString(event.getSpawnLocation().getWorld().getName() + ".pitch")), Float.parseFloat(Main.Instance.spawn.getString(event.getSpawnLocation().getWorld().getName() + ".yaw"))));
+            event.setSpawnLocation(Main.Instance.spawn.getLocation(event.getSpawnLocation().getWorld().getName()));
         }
     }
 
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         if (Main.Instance.spawn.getBoolean("overidespawn")) {
-            event.setRespawnLocation(new Location(event.getRespawnLocation().getWorld(), Main.Instance.spawn.getDouble(event.getRespawnLocation().getWorld().getName() + ".x"), Main.Instance.spawn.getDouble(event.getRespawnLocation().getWorld().getName() + ".y"), Main.Instance.spawn.getDouble(event.getRespawnLocation().getWorld().getName() + ".z"), Float.parseFloat(Main.Instance.spawn.getString(event.getRespawnLocation().getWorld().getName() + ".pitch")), Float.parseFloat(Main.Instance.spawn.getString(event.getRespawnLocation().getWorld().getName() + ".yaw"))));
+            event.setRespawnLocation(Main.Instance.spawn.getLocation(event.getRespawnLocation().getWorld().getName()));
         }
     }
 }
