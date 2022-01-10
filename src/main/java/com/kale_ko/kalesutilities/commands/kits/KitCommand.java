d86ead44f2fd32d5d49c605b1320940daa8e37b9
Kale-Ko <github@kaleko.ga>
2022-01-10 17:34:40 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/commands/kits/KitCommand.java
+++ src/main/java/com/kale_ko/kalesutilities/commands/kits/KitCommand.java
@@ -15,24 +15,6 @@ public class KitCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (Util.hasPermission(sender, "kalesutilities.kit")) {
             if (args.length > 1) {
-                if (sender instanceof Player player) {
-                    List<String> items = Main.Instance.kits.getStringList(args[0]);
-
-                    for (String item : items) {
-                        ItemStack itemStack = new ItemStack(Material.matchMaterial(item), 1);
-
-                        if (itemStack.getType().getEquipmentSlot() != EquipmentSlot.HAND && itemStack.getType().getEquipmentSlot() != EquipmentSlot.OFF_HAND && (player.getEquipment().getItem(itemStack.getType().getEquipmentSlot()) == null || player.getEquipment().getItem(itemStack.getType().getEquipmentSlot()).getType().isAir())) {
-                            player.getEquipment().setItem(itemStack.getType().getEquipmentSlot(), itemStack);
-                        } else {
-                            player.getInventory().addItem(itemStack);
-                        }
-                    }
-
-                    Util.sendMessage(player, Main.Instance.config.getString("message.kit").replace("{kit}", args[0]));
-                } else {
-                    Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
-                }
-            } else if (args.length > 0) {
                 Player player = Main.Instance.getServer().getPlayer(args[0]);
 
                 if (player != null) {
@@ -48,9 +30,27 @@ public class KitCommand implements CommandExecutor {
                         }
                     }
 
-                    Util.sendMessage(player, Main.Instance.config.getString("message.kit").replace("{kit}", args[0]));
+                    Util.sendMessage(player, Main.Instance.config.getString("messages.kit").replace("{kit}", args[0]));
                 } else {
                     Util.sendMessage(sender, Main.Instance.config.getString("messages.playernotfound").replace("{player}", args[0]));
+                }
+            } else if (args.length > 0) {
+                if (sender instanceof Player player) {
+                    List<String> items = Main.Instance.kits.getStringList(args[0]);
+
+                    for (String item : items) {
+                        ItemStack itemStack = new ItemStack(Material.matchMaterial(item), 1);
+
+                        if (itemStack.getType().getEquipmentSlot() != EquipmentSlot.HAND && itemStack.getType().getEquipmentSlot() != EquipmentSlot.OFF_HAND && (player.getEquipment().getItem(itemStack.getType().getEquipmentSlot()) == null || player.getEquipment().getItem(itemStack.getType().getEquipmentSlot()).getType().isAir())) {
+                            player.getEquipment().setItem(itemStack.getType().getEquipmentSlot(), itemStack);
+                        } else {
+                            player.getInventory().addItem(itemStack);
+                        }
+                    }
+
+                    Util.sendMessage(player, Main.Instance.config.getString("messages.kit").replace("{kit}", args[0]));
+                } else {
+                    Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
                 }
             } else {
                 Util.sendMessage(sender, Main.Instance.config.getString("messages.usage").replace("{usage}", Main.Instance.getCommand("kit").getUsage()));
