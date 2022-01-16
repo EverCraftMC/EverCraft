73f6e68a653f1dacee06c2f05f97df5309d834ef
Kale-Ko <github@kaleko.dev>
2022-01-16 17:10:49 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/listeners/CommandSpyListener.java
+++ src/main/java/com/kale_ko/kalesutilities/listeners/CommandSpyListener.java
@@ -11,7 +11,7 @@ public class CommandSpyListener implements Listener {
     @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent event) {
         for (Player player : Main.Instance.getServer().getOnlinePlayers()) {
-            if (player != event.getPlayer() && Util.hasPermission(player, "kalesutilities.commands.staff.commandspy") && Util.getMetadata(player, "receiveCommandSpy").asBoolean()) {
+            if (player != event.getPlayer() && Util.hasPermission(player, "kalesutilities.commands.staff.commandspy") && (Util.hasMetadata(player, "receiveCommandSpy") && Util.getMetadata(player, "receiveCommandSpy").asBoolean())) {
                 Util.sendMessage(player, Main.Instance.config.getString("messages.commandspy").replace("{player}", Util.getPlayerName(event.getPlayer())).replace("{message}", event.getMessage()), true);
             }
         }
