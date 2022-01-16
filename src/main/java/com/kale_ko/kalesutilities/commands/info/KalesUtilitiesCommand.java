87d2ef4e901c1c090e7a5d1840c19090fe2843f1
Kale-Ko <github@kaleko.ga>
2022-01-15 22:01:25 -0500

    Update

--- src/main/java/com/kale_ko/kalesutilities/commands/info/KalesUtilitiesCommand.java
+++ src/main/java/com/kale_ko/kalesutilities/commands/info/KalesUtilitiesCommand.java
@@ -4,20 +4,13 @@ import com.kale_ko.kalesutilities.Main;
 import com.kale_ko.kalesutilities.Util;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommandYamlParser;
 
 public class KalesUtilitiesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (args.length > 0) {
             if (args[0].equalsIgnoreCase("help")) {
-                StringBuilder help = new StringBuilder();
-
-                for (org.bukkit.command.Command pluginCommand : PluginCommandYamlParser.parse(Main.Instance)) {
-                    help.append(pluginCommand.getName() + " - " + pluginCommand.getDescription() + " - " + pluginCommand.getUsage() + "\n");
-                }
-
-                Util.sendMessage(sender, Main.Instance.config.getString("messages.help").replace("{commandList}", help.toString()));
+                Main.Instance.getServer().dispatchCommand(sender, "help");
             } else if (args[0].equalsIgnoreCase("reload")) {
                 Main.Instance.reload();
 
