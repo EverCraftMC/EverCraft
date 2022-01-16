120d9c6beef633e2e643f963820c6a72ec9118a6
Kale-Ko <github@kaleko.dev>
2022-01-15 22:01:25 -0500

    Update

---
+++ src/main/java/com/kale_ko/kalesutilities/commands/info/HelpCommand.java
@@ -0,0 +1,48 @@
+package com.kale_ko.kalesutilities.commands.info;
+
+import com.kale_ko.kalesutilities.Main;
+import com.kale_ko.kalesutilities.Util;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommandYamlParser;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class HelpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (args.length > 0) {
+            Player player = Main.Instance.getServer().getPlayer(args[0]);
+
+            if (player != null) {
+                StringBuilder help = new StringBuilder();
+
+                for (Plugin plugin : Main.Instance.getServer().getPluginManager().getPlugins()) {
+                    for (org.bukkit.command.Command pluginCommand : PluginCommandYamlParser.parse(plugin)) {
+                        if (Util.hasPermission(player, pluginCommand.getPermission())) {
+                            help.append(pluginCommand.getUsage() + " - " + pluginCommand.getDescription() + "\n");
+                        }
+                    }
+                }
+
+                Util.sendMessage(player, Main.Instance.config.getString("messages.help").replace("{commandList}", help.toString()));
+            } else {
+                Util.sendMessage(sender, Main.Instance.config.getString("messages.playernotfound").replace("{player}", args[0]));
+            }
+        } else {
+            StringBuilder help = new StringBuilder();
+
+            for (Plugin plugin : Main.Instance.getServer().getPluginManager().getPlugins()) {
+                for (org.bukkit.command.Command pluginCommand : PluginCommandYamlParser.parse(plugin)) {
+                    if (Util.hasPermission(sender, pluginCommand.getPermission())) {
+                        help.append(pluginCommand.getUsage() + " - " + pluginCommand.getDescription() + "\n");
+                    }
+                }
+            }
+
+            Util.sendMessage(sender, Main.Instance.config.getString("messages.help").replace("{commandList}", help.toString()));
+        }
+
+        return true;
+    }
+}
