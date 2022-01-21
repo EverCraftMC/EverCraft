92cfd7c85e0a1c18f1806f66d0386fd033c01c80
Kale-Ko <github@kaleko.dev>
2022-01-20 19:39:49 -0500

    Update

--- Bungee/src/main/java/com/kale_ko/kalesutilities/bungee/Main.java
+++ Bungee/src/main/java/com/kale_ko/kalesutilities/bungee/Main.java
@@ -1,15 +1,60 @@
 package com.kale_ko.kalesutilities.bungee;
 
+import java.util.logging.Logger;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
+    public static Main Instance;
+
+    public final Logger Console = getLogger();
+
+    public Config config;
+    public Config players;
+
     @Override
     public void onEnable() {
+        Main.Instance = this;
 
+        Console.info("Loading config..");
+
+        config = Config.load("config.yml");
+        players = Config.load("players.yml");
+
+        Console.info("Finished loading config");
+
+        Console.info("Loading commands..");
+
+        Console.info("Finished loading commands");
+
+        Console.info("Loading event listeners..");
+
+        Console.info("Finished loading event listeners");
+
+        Console.info("Finished enabling");
     }
 
     @Override
     public void onDisable() {
-        
+        Console.info("Removing event listeners..");
+
+        getProxy().getPluginManager().unregisterListeners(Main.Instance);
+
+        Console.info("Finished removing event listeners");
+
+        Console.info("Finished disabling");
+    }
+
+    public void reload() {
+        Console.info("Reloading..");
+
+        Console.info("Disabling..");
+
+        this.onDisable();
+
+        Console.info("Enabling..");
+
+        this.onEnable();
+
+        Console.info("Finished reloading");
     }
 }
--- Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/Main.java
+++ Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/Main.java
@@ -52,7 +52,7 @@ import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.LoadOrder;
 import org.bukkit.plugin.java.annotation.plugin.Website;
 
-@Plugin(name = "KalesUtilities", version = "4.3.2")
+@Plugin(name = "KalesUtilities", version = "5.0.0")
 @Description("A custom plugin to run on KalesMC")
 @Website("https://github.com/Kale-Ko/Kales-Utilities-V2")
 @Author("Kale Ko")
