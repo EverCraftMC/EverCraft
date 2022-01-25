e07e52599d1ec6635254da31bc5b15107a7d4223
Kale-Ko <github@kaleko.ga>
2022-01-24 22:16:47 -0500

    Update

--- Shared/src/main/java/com/kale_ko/kalesutilities/shared/mysql/MySQL.java
+++ Shared/src/main/java/com/kale_ko/kalesutilities/shared/mysql/MySQL.java
@@ -11,7 +11,7 @@ public class MySQL {
 
     public MySQL(String url, Integer port, String database, String username, String password) {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + database, username, password);
+            this.connection = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + database, username, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -19,7 +19,7 @@ public class MySQL {
 
     public MySQL(String url, String database, String username, String password) {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://" + url + ":3306/" + database, username, password);
+            this.connection = DriverManager.getConnection("jdbc:mysql://" + url + ":3306/" + database, username, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,7 +28,7 @@ public class MySQL {
     public void createTable(String name, String data) {
         try {
             String createStatement = "CREATE TABLE IF NOT EXISTS " + name + " " + data;
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
 
             statement.executeUpdate(createStatement);
         } catch (SQLException e) {
@@ -45,7 +45,7 @@ public class MySQL {
         StringBuilder ret = new StringBuilder();
 
         try {
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
             ResultSet result = statement.executeQuery(selectStatement);
 
             while (result.next()) {
@@ -69,7 +69,7 @@ public class MySQL {
         String selectStatement = "SELECT * FROM " + table + " " + "WHERE " + condition;
 
         try {
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
             ResultSet result = statement.executeQuery(selectStatement);
 
             while (result.next()) {
@@ -88,7 +88,7 @@ public class MySQL {
     public void insert(String table, String values) {
         try {
             String insertStatement = "INSERT INTO " + table + " VALUES " + values;
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
 
             statement.executeUpdate(insertStatement);
             statement.close();
@@ -100,7 +100,7 @@ public class MySQL {
     public void insert(String table, String keys, String values) {
         try {
             String insertStatement = "INSERT INTO " + table + " " + keys + " VALUES " + values;
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
 
             statement.executeUpdate(insertStatement);
             statement.close();
@@ -112,7 +112,7 @@ public class MySQL {
     public void update(String table, String key, String value, String condition) {
         try {
             String updateStatement = "UPDATE " + table + " SET " + key + " = " + value + " WHERE " + condition;
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
 
             statement.executeUpdate(updateStatement);
             statement.close();
@@ -124,7 +124,7 @@ public class MySQL {
     public void delete(String table, String condition) {
         try {
             String deleteStatement = "DELETE FROM " + table + " WHERE " + condition;
-            Statement statement = connection.createStatement();
+            Statement statement = this.connection.createStatement();
 
             statement.executeUpdate(deleteStatement);
             statement.close();
@@ -135,7 +135,7 @@ public class MySQL {
 
     public void close() {
         try {
-            connection.close();
+            this.connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
