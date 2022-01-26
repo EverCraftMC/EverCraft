e9046271b173135d67a7e2346a547622c1c18b1b
Kale-Ko <github@kaleko.ga>
2022-01-26 13:14:53 -0500

    Update

--- Bungee/src/main/java/com/kale_ko/kalesutilities/bungee/commands/server/HubCommand.java
+++ Bungee/src/main/java/com/kale_ko/kalesutilities/bungee/commands/server/HubCommand.java
@@ -5,6 +5,7 @@ import com.kale_ko.kalesutilities.bungee.Main;
 import com.kale_ko.kalesutilities.bungee.Util;
 import com.kale_ko.kalesutilities.bungee.commands.BungeeCommand;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class HubCommand extends BungeeCommand {
@@ -14,10 +15,34 @@ public class HubCommand extends BungeeCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (sender instanceof ProxiedPlayer player) {
-            player.connect(Main.Instance.getProxy().getServerInfo(Main.Instance.config.getString("config.mainServer")));
+        if (args.length == 0) {
+            if (sender instanceof ProxiedPlayer player) {
+                ServerInfo target = Main.Instance.getProxy().getServerInfo(Main.Instance.config.getString("config.mainServer"));
+
+                if (player.getServer().getInfo() != target) {
+                    player.connect(target);
+                } else {
+                    Util.sendMessage(player, Main.Instance.config.getString("messages.allreadyconnected"));
+                }
+            } else {
+                Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
+            }
         } else {
-            Util.sendMessage(sender, Main.Instance.config.getString("messages.noconsole"));
+            if (Util.hasPermission(sender, "kalesutilities.commands.staff.sudo")) {
+                ProxiedPlayer player = Main.Instance.getProxy().getPlayer(args[0]);
+
+                if (player != null) {
+                    ServerInfo target = Main.Instance.getProxy().getServerInfo(Main.Instance.config.getString("config.mainServer"));
+
+                    if (player.getServer().getInfo() != target) {
+                        player.connect(target);
+                    } else {
+                        Util.sendMessage(player, Main.Instance.config.getString("messages.allreadyconnected"));
+                    }
+                } else {
+                    Util.sendMessage(sender, Main.Instance.config.getString("messages.playernotfound").replace("{player}", args[0]));
+                }
+            }
         }
     }
 }
