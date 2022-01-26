b56724cf764c982e3bf7e9c764c7bbe250dec3e5
Kale-Ko <github@kaleko.dev>
2022-01-25 19:45:52 -0500

    Update

---
+++ Bungee/src/main/java/com/kale_ko/kalesutilities/bungee/listeners/PlayerJoinListener.java
@@ -0,0 +1,22 @@
+package com.kale_ko.kalesutilities.bungee.listeners;
+
+import com.kale_ko.kalesutilities.bungee.Main;
+import com.kale_ko.kalesutilities.bungee.Util;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(ServerConnectEvent event) {
+        if (event.getReason().equals(ServerConnectEvent.Reason.JOIN_PROXY)) {
+            event.setTarget(Main.Instance.getProxy().getServerInfo(Main.Instance.config.getString("config.mainServer")));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PostLoginEvent event) {
+        Util.updatePlayerName(event.getPlayer());
+    }
+}
