ea8227d0b5312965f62f1a2b1709075bf19c0a2b
Kale-Ko <github@kaleko.dev>
2022-02-07 11:34:39 -0500

    Update

--- Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/listeners/ChatListener.java
+++ Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/listeners/ChatListener.java
@@ -10,7 +10,7 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onChatMessage(AsyncPlayerChatEvent event) {
         if (!event.isCancelled()) {
-            Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getFormat().replace("%2$s", event.getMessage()));
+            Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("config.serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getFormat().replace("%2$s", event.getMessage()));
 
             SpigotPlugin.Instance.bot.sendMessage(Util.discordFormating("[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getFormat().replace("%2$s", event.getMessage())));
         }
