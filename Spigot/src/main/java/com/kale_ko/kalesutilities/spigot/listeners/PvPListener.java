0dd024d51cf84feeec02d973bf830a1f61ba1cd9
Kale-Ko <github@kaleko.dev>
2022-02-06 21:13:27 -0500

    Update

---
+++ Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/listeners/PvPListener.java
@@ -0,0 +1,19 @@
+package com.kale_ko.kalesutilities.spigot.listeners;
+
+import com.kale_ko.kalesutilities.spigot.Util;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PvPListener implements Listener {
+    @EventHandler
+    public void onPlayerPvP(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player player1 && event.getDamager() instanceof Player player2) {
+            if ((Util.hasMetadata(player1, "receiveCommandSpy") && Util.getMetadata(player1, "receiveCommandSpy").asBoolean()) || (Util.hasMetadata(player2, "receiveCommandSpy") && Util.getMetadata(player2, "receiveCommandSpy").asBoolean())) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
