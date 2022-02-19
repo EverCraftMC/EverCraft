6061362aa2787afd61b8477ae0dc0fff5bd015ec
Kale-Ko <github@kaleko.dev>
2022-02-18 23:51:10 -0500

    Update

--- Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/commands/warps/WarpCommand.java
+++ Spigot/src/main/java/com/kale_ko/kalesutilities/spigot/commands/warps/WarpCommand.java
@@ -18,9 +18,11 @@ public class WarpCommand extends SpigotCommand {
     public void run(CommandSender sender, String label, String[] args) {
         if (args.length == 1 && sender instanceof Player player) {
             player.teleport(SpigotPlugin.Instance.warps.getSerializable(args[0], Location.class));
-            player.getInventory().clear();
-            for (PotionEffect effect : player.getActivePotionEffects()) {
-                player.removePotionEffect(effect.getType());
+            if (SpigotPlugin.Instance.config.getBoolean("config.clearOnWarp")) {
+                player.getInventory().clear();
+                for (PotionEffect effect : player.getActivePotionEffects()) {
+                    player.removePotionEffect(effect.getType());
+                }
             }
 
             Util.sendMessage(sender, SpigotPlugin.Instance.config.getString("messages.warped").replace("{warp}", args[0]));
@@ -32,9 +34,11 @@ public class WarpCommand extends SpigotCommand {
 
                 if (player != null) {
                     player.teleport(SpigotPlugin.Instance.warps.getSerializable(args[1], Location.class));
-                    player.getInventory().clear();
-                    for (PotionEffect effect : player.getActivePotionEffects()) {
-                        player.removePotionEffect(effect.getType());
+                    if (SpigotPlugin.Instance.config.getBoolean("config.clearOnWarp")) {
+                        player.getInventory().clear();
+                        for (PotionEffect effect : player.getActivePotionEffects()) {
+                            player.removePotionEffect(effect.getType());
+                        }
                     }
 
                     Util.sendMessage(sender, SpigotPlugin.Instance.config.getString("messages.warpedplayer").replace("{warp}", args[1]).replace("{player}", args[0]));
