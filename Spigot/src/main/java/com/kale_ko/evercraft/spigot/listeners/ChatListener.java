6577e44ff109cb09e46c361ff058ed4e38e8dd3d
Kale-Ko <github@kaleko.dev>
2022-03-09 13:23:04 -0500

    Update

--- Spigot/src/main/java/com/kale_ko/evercraft/spigot/listeners/ChatListener.java
+++ Spigot/src/main/java/com/kale_ko/evercraft/spigot/listeners/ChatListener.java
@@ -15,16 +15,12 @@ public class ChatListener implements Listener {
     public void onChatMessage(AsyncPlayerChatEvent event) {
         if (!event.isCancelled()) {
             Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("config.serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getFormat().replace("%2$s", event.getMessage()));
-
-            SpigotPlugin.Instance.bot.sendMessage(Util.discordFormating("[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getFormat().replace("%2$s", event.getMessage())));
         }
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("config.serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getDeathMessage());
-
-        SpigotPlugin.Instance.bot.sendMessage(Util.discordFormating("[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getDeathMessage()));
     }
 
     @EventHandler
@@ -32,12 +28,8 @@ public class ChatListener implements Listener {
         if (((CraftAdvancement) event.getAdvancement()).getHandle().c() != null && event.getPlayer().getWorld().getGameRuleValue(GameRule.ANNOUNCE_ADVANCEMENTS)) {
             if (!((CraftAdvancement) event.getAdvancement()).getHandle().c().e().a().equals("challenge")) {
                 Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("config.serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getPlayer().getName() + " has made the advancement [" + ((CraftAdvancement) event.getAdvancement()).getHandle().c().a().getString() + "]");
-
-                SpigotPlugin.Instance.bot.sendMessage(Util.discordFormating("[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getPlayer().getName() + " has made the advancement [" + ((CraftAdvancement) event.getAdvancement()).getHandle().c().a().getString() + "]"));
             } else {
                 Util.messageBungee("globalChat", SpigotPlugin.Instance.config.getString("config.serverName"), "[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getPlayer().getName() + " has completed the challenge [" + ((CraftAdvancement) event.getAdvancement()).getHandle().c().a().getString() + "]");
-
-                SpigotPlugin.Instance.bot.sendMessage(Util.discordFormating("[" + SpigotPlugin.Instance.config.getString("config.serverName") + "] " + event.getPlayer().getName() + " has completed the challenge [" + ((CraftAdvancement) event.getAdvancement()).getHandle().c().a().getString() + "]"));
             }
         }
     }
