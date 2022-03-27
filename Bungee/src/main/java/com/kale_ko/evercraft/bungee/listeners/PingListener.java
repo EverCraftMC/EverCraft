8f3f54a50094ef0013d0c44ad808b17996eebc01
Kale <github@kaleko.ga>
2022-03-27 11:25:01 -0400

    Update

--- Bungee/src/main/java/com/kale_ko/evercraft/bungee/listeners/PingListener.java
+++ Bungee/src/main/java/com/kale_ko/evercraft/bungee/listeners/PingListener.java
@@ -23,16 +23,19 @@ public class PingListener extends BungeeListener {
             } else {
                 ping.setDescriptionComponent(new TextComponent(TextComponent.fromLegacyText(BungeeMain.getInstance().getProxy().getConfigurationAdapter().getListeners().iterator().next().getMotd())));
             }
+
+            List<PlayerInfo> sample = new ArrayList<PlayerInfo>();
+            for (ProxiedPlayer player : BungeeMain.getInstance().getProxy().getPlayers()) {
+                sample.add(new PlayerInfo(player.getName(), player.getUniqueId()));
+            }
+            Players players = new Players(BungeeMain.getInstance().getProxy().getConfigurationAdapter().getListeners().iterator().next().getMaxPlayers(), BungeeMain.getInstance().getProxy().getOnlineCount(), sample.toArray(new PlayerInfo[] {}));
+            ping.setPlayers(players);
         } else {
             ping.setDescriptionComponent(new TextComponent(TextComponent.fromLegacyText(BungeeMain.getInstance().getProxy().getConfigurationAdapter().getListeners().iterator().next().getMotd().split("\n")[0] + "\n" + TextFormatter.translateColors(BungeeMain.getInstance().getPluginMessages().getString("moderation.maintenance.motd")))));
-        }
 
-        List<PlayerInfo> sample = new ArrayList<PlayerInfo>();
-        for (ProxiedPlayer player : BungeeMain.getInstance().getProxy().getPlayers()) {
-            sample.add(new PlayerInfo(player.getName(), player.getUniqueId()));
+            Players players = new Players(BungeeMain.getInstance().getProxy().getConfigurationAdapter().getListeners().iterator().next().getMaxPlayers(), 0, new PlayerInfo[] {});
+            ping.setPlayers(players);
         }
-        Players players = new Players(BungeeMain.getInstance().getProxy().getConfigurationAdapter().getListeners().iterator().next().getMaxPlayers(), BungeeMain.getInstance().getProxy().getOnlineCount(), sample.toArray(new PlayerInfo[] {}));
-        ping.setPlayers(players);
 
         event.setResponse(ping);
     }
