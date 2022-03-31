3535cc62009b89b4960759901434152bfd52c63c
Kale-Ko <github@kaleko.dev>
2022-03-31 16:03:29 -0400

    Check args length

--- Bungee/src/main/java/com/kale_ko/evercraft/bungee/commands/player/SpigotCommandCommand.java
+++ Bungee/src/main/java/com/kale_ko/evercraft/bungee/commands/player/SpigotCommandCommand.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import com.kale_ko.evercraft.bungee.BungeeMain;
 import com.kale_ko.evercraft.bungee.commands.BungeeCommand;
+import com.kale_ko.evercraft.bungee.util.formatting.ComponentFormatter;
+import com.kale_ko.evercraft.shared.util.formatting.TextFormatter;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
@@ -15,21 +18,23 @@ public class SpigotCommandCommand extends BungeeCommand {
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        // TODO Check args length
+        if (args.length > 0) {
+            if (sender instanceof ProxiedPlayer player) {
+                StringBuilder command = new StringBuilder();
 
-        if (sender instanceof ProxiedPlayer player) {
-            StringBuilder command = new StringBuilder();
+                for (String arg : args) {
+                    command.append(arg + " ");
+                }
 
-            for (String arg : args) {
-                command.append(arg + " ");
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("crossCommand");
+                out.writeUTF(player.getUniqueId().toString());
+                out.writeUTF(command.substring(0, command.length() - 1));
+
+                player.getServer().sendData("BungeeCord", out.toByteArray());
             }
-
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("crossCommand");
-            out.writeUTF(player.getUniqueId().toString());
-            out.writeUTF(command.substring(0, command.length() - 1));
-
-            player.getServer().sendData("BungeeCord", out.toByteArray());
+        } else {
+            sender.sendMessage(ComponentFormatter.stringToComponent(TextFormatter.translateColors(BungeeMain.getInstance().getPluginMessages().getString("error.invalidArgs"))));
         }
     }
 
