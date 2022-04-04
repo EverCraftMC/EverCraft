2635a39fac32c8c9841781adb023e394fabe1a91
Kale-Ko <github@kaleko.dev>
2022-04-03 23:52:23 -0400

    Fix mutes and add spawn overide and warp clearing

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/commands/warp/WarpCommand.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/commands/warp/WarpCommand.java
@@ -24,6 +24,11 @@ public class WarpCommand extends SpigotCommand {
                 if (SpigotMain.getInstance().getWarps().getSerializable(args[0], SerializableLocation.class) != null) {
                     player.teleport(SpigotMain.getInstance().getWarps().getSerializable(args[0], SerializableLocation.class).toBukkitLocation());
 
+                    if (SpigotMain.getInstance().getPluginConfig().getBoolean("warp.clearonwarp")) {
+                        player.getInventory().clear();
+                        player.getActivePotionEffects().clear();
+                    }
+
                     player.sendMessage(ComponentFormatter.stringToComponent(TextFormatter.translateColors(SpigotMain.getInstance().getPluginMessages().getString("warp.warped").replace("{warp}", args[0]))));
                 } else {
                     player.sendMessage(ComponentFormatter.stringToComponent(TextFormatter.translateColors(SpigotMain.getInstance().getPluginMessages().getString("warp.notFound").replace("{warp}", args[0]))));
