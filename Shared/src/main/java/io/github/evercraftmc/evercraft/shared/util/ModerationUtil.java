e4bdad58859ebdb10b954b3b9671d47c83050ca6
Kale-Ko <github@kaleko.ga>
2022-04-06 00:28:43 -0400

    Update version

--- Shared/src/main/java/io/github/evercraftmc/evercraft/shared/util/ModerationUtil.java
+++ Shared/src/main/java/io/github/evercraftmc/evercraft/shared/util/ModerationUtil.java
@@ -2,7 +2,7 @@ package io.github.evercraftmc.evercraft.shared.util;
 
 public class ModerationUtil {
     private static final String[] bannedRegexes = new String[] { "ass(hole)?(s)?", "bastard(s)?", "bitch(es)?", "blowjob(s)?", "cock(s)?", "cocksucker(s)?", "cunt(s)?", "dick(s)?", "fu(c)?k(er)?(s)?", "fuck(ing)?", "motherfuck(er)?(s)?", "nigg(a)?(s)?", "nig(g)?er(s)?", "pp", "porn", "pornstar(s)?", "prostitute(s)?", "pussy", "pussies", "shit(s)?", "shit(er)?", "slut(s)?", "whore(s)?" };
-    private static final String[] bannedWords = new String[] { "fu", "testisaidso" };
+    private static final String[] bannedWords = new String[] { "fu" };
 
     public static boolean isInappropriateString(String string) {
         for (String word : string.split(" ")) {
