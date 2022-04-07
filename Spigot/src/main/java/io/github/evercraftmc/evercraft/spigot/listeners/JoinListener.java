38d565acaba81a87543c393865e0737f8cbbea89
Kale-Ko <github@kaleko.dev>
2022-04-06 21:25:26 -0400

    Fix server name

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/JoinListener.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/JoinListener.java
@@ -4,6 +4,8 @@ import io.github.evercraftmc.evercraft.shared.util.formatting.TextFormatter;
 import io.github.evercraftmc.evercraft.spigot.SpigotMain;
 import io.github.evercraftmc.evercraft.spigot.util.formatting.ComponentFormatter;
 import io.github.evercraftmc.evercraft.spigot.util.types.SerializableLocation;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -15,6 +17,12 @@ import net.luckperms.api.LuckPermsProvider;
 public class JoinListener extends SpigotListener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if (SpigotMain.getInstance().getServerName().equals("unknown")) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("GetServer");
+            SpigotMain.getInstance().getServer().sendPluginMessage(SpigotMain.getInstance(), "BungeeCord", out.toByteArray());
+        }
+
         event.joinMessage(Component.empty());
 
         event.getPlayer().displayName(ComponentFormatter.stringToComponent(TextFormatter.translateColors(LuckPermsProvider.get().getUserManager().getUser(event.getPlayer().getUniqueId()).getCachedData().getMetaData().getPrefix() + SpigotMain.getInstance().getData().getString("players." + event.getPlayer().getUniqueId() + ".nickname"))));
