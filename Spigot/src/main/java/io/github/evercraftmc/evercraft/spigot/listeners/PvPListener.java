98402574c534db9f5ab0156f08dde691879c5e54
Kale-Ko <github@kaleko.dev>
2022-04-12 17:19:56 -0400

    Check for damage from things the player launches

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/PvPListener.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/PvPListener.java
@@ -1,6 +1,8 @@
 package io.github.evercraftmc.evercraft.spigot.listeners;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import io.github.evercraftmc.evercraft.spigot.SpigotMain;
@@ -8,9 +10,19 @@ import io.github.evercraftmc.evercraft.spigot.SpigotMain;
 public class PvPListener extends SpigotListener {
     @EventHandler
     public void onPlayerPvP(EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof Player player && event.getDamager() instanceof Player player2) {
-            if (SpigotMain.getInstance().getData().getBoolean("players." + player.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName()) || SpigotMain.getInstance().getData().getBoolean("players." + player2.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName())) {
-                event.setCancelled(true);
+        if (event.getEntity() instanceof Player player) {
+            if (event.getDamager() instanceof Player player2) {
+                if (SpigotMain.getInstance().getData().getBoolean("players." + player.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName()) || SpigotMain.getInstance().getData().getBoolean("players." + player2.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName())) {
+                    event.setCancelled(true);
+                }
+            } else if (event.getDamager() instanceof Projectile projectile && projectile.getShooter() instanceof Player player2) {
+                if (SpigotMain.getInstance().getData().getBoolean("players." + player.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName()) || SpigotMain.getInstance().getData().getBoolean("players." + player2.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName())) {
+                    event.setCancelled(true);
+                }
+            } else if (event.getDamager() instanceof Tameable tameable && (tameable.getOwner() != null && tameable.getOwner() instanceof Player player2)) {
+                if (SpigotMain.getInstance().getData().getBoolean("players." + player.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName()) || SpigotMain.getInstance().getData().getBoolean("players." + player2.getUniqueId() + ".passive." + SpigotMain.getInstance().getServerName())) {
+                    event.setCancelled(true);
+                }
             }
         }
     }
