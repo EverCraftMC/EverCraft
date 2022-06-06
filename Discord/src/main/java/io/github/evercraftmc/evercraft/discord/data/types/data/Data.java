010c7419561dfc3a3c2264fb5b3cae83b347fbca
Kale-Ko <github@kaleko.dev>
2022-06-05 20:28:04 -0400

    Add reaction roles

--- Discord/src/main/java/io/github/evercraftmc/evercraft/discord/data/types/data/Data.java
+++ Discord/src/main/java/io/github/evercraftmc/evercraft/discord/data/types/data/Data.java
@@ -9,4 +9,6 @@ import io.github.evercraftmc.evercraft.discord.data.DataParseable;
 public class Data extends DataParseable {
     public List<ModCase> history = new ArrayList<ModCase>();
     public Map<String, List<Warning>> warnings = new HashMap<String, List<Warning>>();
+
+    public List<ReactionRole> reactions = new ArrayList<ReactionRole>();
 }
