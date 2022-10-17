5ecc49146d5aba8f97cf03f3ff0826535d12c34e
Kale-Ko <github@kaleko.dev>
2022-10-17 19:26:25 -0400

    Finish creative item fix

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/CreativeItemListener.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/listeners/CreativeItemListener.java
@@ -1,19 +1,103 @@
 package io.github.evercraftmc.evercraft.spigot.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.item.ItemStack;
 
 public class CreativeItemListener extends SpigotListener {
     @EventHandler
-    public void onItemCreate(InventoryCreativeEvent event) {
-        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getCurrentItem());
-        NBTTagCompound nbtCompound = itemStack.u();
+    public void onItemCreate(InventoryClickEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getCursor());
+        NBTTagCompound nbtCompound = itemStack.v();
 
-        if (nbtCompound.b("BlockTag") || nbtCompound.b("EntityTag") || nbtCompound.b("BlockState")) {
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
             event.setCancelled(true);
+
+            event.getCursor().setType(Material.AIR);
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(InventoryMoveItemEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getItem());
+        NBTTagCompound nbtCompound = itemStack.v();
+
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+            event.setCancelled(true);
+
+            event.getItem().setType(Material.AIR);
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(ItemSpawnEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getEntity().getItemStack());
+        NBTTagCompound nbtCompound = itemStack.v();
+
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+            event.setCancelled(true);
+
+            event.getEntity().remove();
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(PlayerDropItemEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getItemDrop().getItemStack());
+        NBTTagCompound nbtCompound = itemStack.v();
+
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+            event.setCancelled(true);
+
+            event.getItemDrop().remove();
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(InventoryPickupItemEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getItem().getItemStack());
+        NBTTagCompound nbtCompound = itemStack.v();
+
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+            event.setCancelled(true);
+
+            event.getItem().remove();
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(PlayerItemHeldEvent event) {
+        ItemStack itemStack = CraftItemStack.asNMSCopy(event.getPlayer().getInventory().getItem(event.getNewSlot()));
+        NBTTagCompound nbtCompound = itemStack.v();
+
+        if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+            event.setCancelled(true);
+
+            event.getPlayer().getInventory().setItem(event.getNewSlot(), null);
+        }
+    }
+
+    @EventHandler
+    public void onItemCreate(PlayerInteractEvent event) {
+        if ((event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) && event.getItem() != null) {
+            ItemStack itemStack = CraftItemStack.asNMSCopy(event.getItem());
+            NBTTagCompound nbtCompound = itemStack.v();
+
+            if (nbtCompound.e("BlockTag") || nbtCompound.e("EntityTag") || nbtCompound.e("BlockState")) {
+                event.getItem().setType(Material.AIR);
+
+                event.setCancelled(true);
+            }
         }
     }
 }
