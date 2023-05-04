b77bfc9995e75d5a957da5b4f79fc917e655c758
Kale-Ko <github@kaleko.dev>
2023-05-04 17:51:40 -0400

    Restore spleef arena

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/games/spleef/SpleefGame.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/games/spleef/SpleefGame.java
@@ -1,8 +1,13 @@
 package io.github.evercraftmc.evercraft.spigot.games.spleef;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 import io.github.evercraftmc.evercraft.spigot.commands.kit.KitCommand;
 import io.github.evercraftmc.evercraft.spigot.games.RoundedGame;
 
@@ -11,8 +16,10 @@ public class SpleefGame extends RoundedGame {
 
     protected Material deathBlock;
 
+    protected Map<Location, Material> brokenBlocks = new HashMap<Location, Material>();
+
     public SpleefGame(String name, String warpName, Integer countdownLength, String kitName, Material deathBlock) {
-        super(name, warpName, 0, Integer.MAX_VALUE, countdownLength);
+        super(name, warpName, 2, Integer.MAX_VALUE, countdownLength);
 
         this.kitName = kitName;
 
@@ -20,12 +27,33 @@ public class SpleefGame extends RoundedGame {
     }
 
     @Override
+    public void startNoCountdown() {
+        super.startNoCountdown();
+
+        if (this.kitName != null) {
+            for (Player player : this.players) {
+                new KitCommand("kit", null, Arrays.asList(), null, true).run(player, new String[] { kitName });
+            }
+        }
+    }
+
+    @Override
     public void join(Player player) {
         super.join(player);
 
-        if (this.kitName != null) {
-            new KitCommand("kit", null, Arrays.asList(), null, true).run(player, new String[] { kitName });
+        if (this.players.size() >= this.minPlayers) {
+            this.start();
         }
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+
+        for (Map.Entry<Location, Material> entry : this.brokenBlocks.entrySet()) {
+            entry.getKey().getBlock().setType(entry.getValue());
+        }
+        this.brokenBlocks.clear();
     }
 
     @Override
@@ -35,6 +63,20 @@ public class SpleefGame extends RoundedGame {
         for (Player player : this.players) {
             if (player.getLocation().getBlock().getType() == this.deathBlock) {
                 player.damage(Integer.MAX_VALUE);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (this.players.contains(event.getPlayer())) {
+            if (started && event.getBlock().getType() == Material.SNOW_BLOCK) {
+                brokenBlocks.put(event.getBlock().getLocation(), event.getBlock().getType());
+
+                event.setDropItems(false);
+                event.setExpToDrop(0);
+            } else {
+                event.setCancelled(true);
             }
         }
     }
