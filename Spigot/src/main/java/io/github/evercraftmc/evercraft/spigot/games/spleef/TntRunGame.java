d2978f93ec7f396e37b3241952486003def11a50
Kale-Ko <github@kaleko.dev>
2023-05-04 17:51:49 -0400

    TntRun!

--- Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/games/spleef/TntRunGame.java
+++ Spigot/src/main/java/io/github/evercraftmc/evercraft/spigot/games/spleef/TntRunGame.java
@@ -1,8 +1,9 @@
 package io.github.evercraftmc.evercraft.spigot.games.spleef;
 
 import org.bukkit.Material;
-import org.bukkit.block.BlockFace;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import io.github.evercraftmc.evercraft.spigot.SpigotMain;
 
 public class TntRunGame extends SpleefGame {
     public TntRunGame(String name, String warpName, Integer countdownLength, Material deathBlock) {
@@ -14,9 +15,26 @@ public class TntRunGame extends SpleefGame {
         super.tick();
 
         for (Player player : this.players) {
-            // TODO Get closest block instead
-            if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.TNT) {
-                player.getLocation().getBlock().getRelative(BlockFace.DOWN).setType(Material.AIR);
+            Block block = null;
+            for (int z = -1; z <= 1; z++) {
+                for (int x = -1; x <= 1; x++) {
+                    Block block2 = player.getLocation().add(x, -0.5, z).getBlock();
+
+                    if (block2.getType() == Material.TNT && (block == null || block2.getLocation().distance(player.getLocation()) <= block.getLocation().distance(player.getLocation()))) {
+                        block = block2;
+                    }
+                }
+            }
+
+            final Block block2 = block;
+
+            if (block2 != null) {
+                SpigotMain.getInstance().getServer().getScheduler().runTaskLater(SpigotMain.getInstance(), () -> {
+                    if (started && block2.getType() == Material.TNT) {
+                        brokenBlocks.put(block2.getLocation(), block2.getType());
+                        block2.setType(Material.AIR);
+                    }
+                }, 2);
             }
         }
     }
