688511d3c08d8df91c41d256ae9e3127bc23114a
Kale-Ko <github@kaleko.dev>
2023-10-19 08:15:04 -0400

    Woops wrong event

--- Moderation/src/main/java/io/github/evercraftmc/moderation/listeners/MaintenanceListener.java
+++ Moderation/src/main/java/io/github/evercraftmc/moderation/listeners/MaintenanceListener.java
@@ -3,7 +3,7 @@ package io.github.evercraftmc.moderation.listeners;
 import io.github.evercraftmc.core.api.events.ECHandler;
 import io.github.evercraftmc.core.api.events.ECHandlerOrder;
 import io.github.evercraftmc.core.api.events.ECListener;
-import io.github.evercraftmc.core.api.events.player.PlayerChatEvent;
+import io.github.evercraftmc.core.api.events.player.PlayerJoinEvent;
 import io.github.evercraftmc.core.impl.util.ECTextFormatter;
 import io.github.evercraftmc.moderation.ModerationModule;
 
@@ -15,7 +15,7 @@ public class MaintenanceListener implements ECListener {
     }
 
     @ECHandler(order=ECHandlerOrder.BEFORE)
-    public void onPlayerChat(PlayerChatEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         if (parent.getPlugin().getPlayerData().maintenance && !event.getPlayer().hasPermission("evercraft.moderation.commands.maintenance.bypass")) {
             event.setCancelled(true);
             event.setCancelReason(ECTextFormatter.translateColors("&cThe server is currently in maintenance mode\nCheck back later!"));
