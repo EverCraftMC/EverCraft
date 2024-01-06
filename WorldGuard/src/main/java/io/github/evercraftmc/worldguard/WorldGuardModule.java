7709d58cfe2c28c5049d2ca2cdfaeb526b5f06aa
Kale-Ko <github@kaleko.dev>
2024-01-05 19:28:38 -0500

    Make WorldGuard a SPIGOT only module

--- WorldGuard/src/main/java/io/github/evercraftmc/worldguard/WorldGuardModule.java
+++ WorldGuard/src/main/java/io/github/evercraftmc/worldguard/WorldGuardModule.java
@@ -1,44 +1,24 @@
 package io.github.evercraftmc.worldguard;
 
-import io.github.evercraftmc.core.ECPlugin;
 import io.github.evercraftmc.core.api.ECModule;
-import io.github.evercraftmc.core.api.ECModuleInfo;
 import io.github.evercraftmc.core.api.commands.ECCommand;
 import io.github.evercraftmc.core.api.events.ECListener;
+import io.github.evercraftmc.core.impl.ECEnvironment;
+import io.github.evercraftmc.worldguard.listeners.WorldGuardListener;
 import java.util.ArrayList;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
-public class WorldGuardModule implements ECModule {
-    protected ECModuleInfo info;
-
-    protected ECPlugin plugin;
-
+public class WorldGuardModule extends ECModule {
     protected @NotNull List<ECCommand> commands = new ArrayList<>();
     protected @NotNull List<ECListener> listeners = new ArrayList<>();
 
-    public @NotNull String getName() {
-        return this.getInfo().getName();
-    }
-
-    public @NotNull ECModuleInfo getInfo() {
-        return this.info;
-    }
-
-    public void setInfo(@NotNull ECModuleInfo info) {
-        this.info = info;
-    }
-
-    public @NotNull ECPlugin getPlugin() {
-        return this.plugin;
-    }
-
-    public void setPlugin(@NotNull ECPlugin plugin) {
-        this.plugin = plugin;
-    }
-
     public void load() {
+        if (this.plugin.getEnvironment() != ECEnvironment.SPIGOT) {
+            throw new RuntimeException("Unsupported environment");
+        }
 
+        this.listeners.add(this.plugin.getServer().getEventManager().register(new WorldGuardListener()));
     }
 
     public void unload() {
---
+++ WorldGuard/src/main/java/io/github/evercraftmc/worldguard/listeners/WorldGuardListener.java
@@ -0,0 +1,18 @@
+package io.github.evercraftmc.worldguard.listeners;
+
+import io.github.evercraftmc.core.api.events.ECHandler;
+import io.github.evercraftmc.core.api.events.ECListener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class WorldGuardListener implements ECListener {
+    @ECHandler
+    public void onPlayerBlockPlace(BlockPlaceEvent event) {
+
+    }
+
+    @ECHandler
+    public void onPlayerBlockBreak(BlockBreakEvent event) {
+
+    }
+}
